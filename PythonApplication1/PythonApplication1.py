4d7073061b7d537074662c773797e75c1664aa31
Yoshihiro Ohta <futotta2004@hotmail.com>
2023-03-24 17:02:37 +0900

    エラーが起きたまま

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,2 +1,5 @@
+﻿import sys
+#print(sys.path)
+sys.path.append('C:/Users/Admin/AppData/Roaming/Python/Python311/site-packages')
 from PIL import Image
 Image.open('input.png').convert('RGB').save('output.jpg', quality=90)
